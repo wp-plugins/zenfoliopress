b0eee08095978f62c4cb77f09731741dcb10b066
nusbaum <nusbaum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-31 04:22:27 +0000

    tagging version 0.0.1
    git-svn-id: https://plugins.svn.wordpress.org/zenfoliopress/tags/0.0.1@431018 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Zenfolio.php
@@ -0,0 +1,156 @@
+<?php
+class Zenfolio {
+	private static $url = 'www.zenfolio.com/api/1.4/zfapi.asmx';
+	private static $userAgent = 'ZenfolioPress v001';
+	var $token = null;
+	var $loginName = null;
+	var $cacheSeconds = 3600;
+
+	function __construct () {
+		/* removed caching for now */
+	}
+
+	private function call($method,$params,$try = false,$secure = false) {
+		$request['method'] = $method;
+		$request['params'] = $params;
+		$request['id'] = 1;
+		$bodyString = json_encode($request);
+		//trigger_error($bodyString,E_USER_NOTICE);
+		$bodyLength = strlen($bodyString);
+		$headers = array();
+		$headers[] = 'Host: www.zenfolio.com';
+		$headers[] = 'X-Zenfolio-User-Agent: '.self::$userAgent;
+		if($this->token) {
+			$headers[] = 'X-Zenfolio-Token: '.$this->token;
+		}
+		$headers[] = 'Content-Type: application/json';
+		$headers[] = 'Content-Length: '.$bodyLength."\r\n";
+		$headers[] = $bodyString;
+
+		//$bodyString = '{"method": "'.$method.'","params": '.$params.',"id": 1}';
+		$protocol = $secure ? 'https' : 'http';
+
+		$curl_connection = curl_init($protocol.'://'.self::$url);
+		curl_setopt($curl_connection, CURLOPT_USERAGENT, self::$userAgent);
+		curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($curl_connection, CURLOPT_POST, true);
+		curl_setopt($curl_connection, CURLOPT_HEADER, TRUE);
+		curl_setopt($curl_connection, CURLOPT_HTTPHEADER, $headers);
+
+		$result = curl_exec($curl_connection);
+		curl_close($curl_connection);
+		if ($result) {
+			//trigger_error($result,E_USER_NOTICE);
+			$json = json_decode(substr($result, strpos($result, '{') - 1));
+			$result = $json->result;
+			if(!$result) {
+				if($try) {
+					throw new ZenfolioException($json->error->message,$json->error->code);
+				} else {
+					return null;
+				}
+			}
+			return $result;
+		} else {
+			if($try) {
+				throw new Exception('curl connection failed');
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public function authenticatePlain($loginName, $password) {
+		$params = array($loginName,$password);
+		try {
+			$this->token = $this->call('AuthenticatePlain',$params,true,true);
+			$this->loginName = $loginName;
+		} catch (ZenfolioException $e) {
+			trigger_error($e->getMessage(),E_USER_ERROR);
+			throw $e;
+		}
+		return $this->token;
+	}
+
+	public function getPopularPhotos($offset, $max) {
+		$params = array($offset,$max);
+		return $this->call('GetPopularPhotos',$params);
+	}
+
+	public function loadGroup($groupId,$level='LEVEL1',$includeChildren=false) {
+		$params = array($groupId,$level,$includeChildren);
+		$group = $this->call('LoadGroup',$params,true);
+		return $group;
+	}
+
+	public function loadGroupHierarchy($loginName) {
+		$params = array($loginName);
+		$groupHierarchy = $this->call('LoadGroupHierarchy',$params);
+		return $groupHierarchy;
+	}
+
+	public function loadPhoto($photoId,$level='LEVEL1') {
+		$params = array($photoId,$level);
+		$photo = $this->call('LoadPhoto',$params);
+		return $photo;
+	}
+
+	public function loadPhotoSet($photoSetId,$level='LEVEL1',$includePhotos=false) {
+		$params = array($photoSetId,$level,$includePhotos ? true : false);
+		$photoSet = $this->call('LoadPhotoSet',$params);
+		if($includePhotos > 1) {
+			foreach ($photoSet->Photos as &$photo) {
+				$photo = $this->loadPhoto($photo->Id,2);
+			}
+		}
+		return $photoSet;
+	}
+
+	public function loadPublicProfile($loginName) {
+		$params = array($loginName);
+		$publicProfile = $this->call('LoadPublicProfile',$params);
+		return $publicProfile;
+	}
+
+}
+
+class ZenfolioException extends Exception {
+	const E_ACCOUNTLOCKED = 1;
+	const E_CONNECTIONISNOTSECURE = 2;
+	const E_DUPLICATEEMAIL = 3;
+	const E_DUPLICATELOGINNAME = 4;
+	const E_INVALIDCREDENTIALS = 5;
+	const E_INVALIDFILEFORMAT = 6;
+	const E_INVALIDPARAM = 7;
+	const E_FILESIZEQUOTAEXCEEDED = 8;
+	const E_NOSUCHOBJECT = 9;
+	const E_NOTAUTHENTICATED = 10;
+	const E_NOTAUTHORIZED = 11;
+	const E_STORAGEQUOTAEXCEEDED = 12;
+	const E_UNSPECIFIEDERROR = 13;
+	private static $codes = array(
+	'E_ACCOUNTLOCKED' => self::E_ACCOUNTLOCKED,
+	'E_ACCOUNTLOCKED' => self::E_ACCOUNTLOCKED,
+	'E_DUPLICATEEMAIL' => self::E_DUPLICATEEMAIL,
+	'E_DUPLICATELOGINNAME' => self::E_DUPLICATELOGINNAME,
+	'E_INVALIDCREDENTIALS' => self::E_INVALIDCREDENTIALS,
+	'E_INVALIDFILEFORMAT' => self::E_INVALIDFILEFORMAT,
+	'E_INVALIDPARAM' => self::E_INVALIDPARAM,
+	'E_FILESIZEQUOTAEXCEEDED' => self::E_FILESIZEQUOTAEXCEEDED,
+	'E_NOSUCHOBJECT' => self::E_NOSUCHOBJECT,
+	'E_NOTAUTHENTICATED' => self::E_NOTAUTHENTICATED,
+	'E_NOTAUTHORIZED' => self::E_NOTAUTHORIZED,
+	'E_STORAGEQUOTAEXCEEDED' => self::E_STORAGEQUOTAEXCEEDED,
+	'E_UNSPECIFIEDERROR' => self::E_UNSPECIFIEDERROR);
+
+	function __construct ($message, $code = 0, $previous = NULL) {
+		$this->message = $message;
+		if(key_exists($code,self::$codes)) {
+			$this->code = self::$codes[$code];
+		} else {
+			$code = 0;
+		}
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== ZenfolioPress ===
+Contributors: nusbaum
+Donate link: http://www.zenfoliopress.com/
+Tags: comments, spam
+Requires at least: 2.7
+Tested up to: 3.2
+Stable tag: 0.0.1
+
+The ZenfolioPress plugin for WordPress allows users to easily incorporate their Zenfolio hosted
+photos, galleries and collection into their WordPress site.
+
+== Description ==
+
+__Warning...__ This early release of the plugin is being posted for testing and input from potential users.
+It is quite likely that something may not work right in different hosting environment or using different 
+browsers. I will fix bug and add enhancements as quick as possible.
+
+
+== Installation ==
+
+1. Extract the contents of the zip archive to the '/wp-content/plugins/' directory or 
+install the plugin from your WordPress dashboard -> plugins -> add new menu
+1. Activate the plugin through the \'Plugins menu\' in WordPress
+1. Configure the plugin using the ZenfoliioPress settings page
+
+= Displaying Photos =
+
+Insert the code [ZFP_Photo id='nnnn'] to display a photo from your Zenfolio account. 
+The photo id can be found at the end of the URL displayed in your web browser.
+
+> http://www.davidnusbaum.com/individuals/h6913aba#__h6913aba__
+
+== Displaying Galleries and Collections ==
+
+Insert the code [ZFP_PhotoSet id='nnnn'] to display a Zenfolio gallery or collection. 
+The id can be found at the end of the URL displayed in your web browser.
+
+> http://www.davidnusbaum.com/p__444438099__
+
+If you have a friendly URL for a gallery or collection you can find the id by going to the edit view.
+
+> http://www.zenfolio.com/nusbaum/p__444438099__/edit
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+Will add soon...
+
+== Changelog ==
+
+= 0.0.1 =
+An alpha level release published for testing and getting input from potential users.
+
+== To Do ==
+
+1. Add clear overlay gif to at least slow down efforts to right click and save your images.
+2. Add alternative actions for when images and thumbnails are clicked.
