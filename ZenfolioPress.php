8e84a1a298e9569ac28e78857d6f32a25404bbc6
nusbaum <nusbaum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-02 03:59:16 +0000

    added options to control photo and thumbnail links
    git-svn-id: https://plugins.svn.wordpress.org/zenfoliopress/trunk@432039 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ZenfolioPress.php
+++ ZenfolioPress.php
@@ -27,8 +27,9 @@
 
 /* admin actions */
 if ( is_admin() ){
-	add_action('admin_menu', array('ZenfolioPress','adminMenu'));
-	add_action('admin_init', array('ZenfolioPress','registerSettings'));
+	include_once 'ZenfolioPressAdmin.php';
+	add_action('admin_menu', array('ZenfolioPressAdmin','adminMenu'));
+	add_action('admin_init', array('ZenfolioPressAdmin','registerSettings'));
 } else {
 	add_action('wp_print_styles',array('ZenfolioPress','loadStyleSheet'));
 	/* add filters for photo and photoset shortcuts */
@@ -41,6 +42,7 @@ class ZenfolioPress {
 	const STYLE_VERSION = 'v001';
 	private static $options = null;
 	private static $sizes = null;
+
 
 	public static function getFrameCellStyle($size) {
 		$sizes = self::getSizes();
@@ -66,7 +68,11 @@ class ZenfolioPress {
 			/* set default options */
 			$default['userName'] = '';
 			$default['photoSize']='3';
+			$default['photoAction']='1';
+			$default['photoTarget']='_self';
 			$default['thumbSize']='10';
+			$default['thumbAction']='2';
+			$default['thumbTarget']='_self';
 
 			/* update defauls with current options */
 			$options = get_option('ZFP_Settings');
@@ -107,107 +113,6 @@ class ZenfolioPress {
 		}
 	}
 
-	public static function adminMenu() {
-		add_options_page('ZenfolioPress Options', 'ZenfolioPress', 'manage_options', 'zfp_options', array('ZenfolioPress','showOptions'));
-	}
-
-	public static function showOptions() {
-		if (!current_user_can('manage_options'))  {
-			wp_die( __('You do not have sufficient permissions to access this page.') );
-		}
-		include 'options.html';
-	}
-
-	public static function registerSettings() {
-		register_setting( 'ZFP_Settings', "ZFP_Settings",array('ZenfolioPress','validateOptions') );
-		add_settings_section('ZFP_ZenfolioSettings', 'Zenfolio Settings',array('ZenfolioPress','zenfolioSettingsHTML'), 'ZenfolioPress');
-		add_settings_field('userName', 'Zenfolio User Name', array('ZenfolioPress','userNameHTML'), 'ZenfolioPress', 'ZFP_ZenfolioSettings');
-
-
-		add_settings_section('ZFP_PhotoSettings', 'Photo Settings',array('ZenfolioPress','photoSettingsHTML'), 'ZenfolioPress');
-		add_settings_field('photoSize', 'Default Photo Size', array('ZenfolioPress','photoSizeHTML'), 'ZenfolioPress', 'ZFP_PhotoSettings');
-
-		add_settings_section('ZFP_PhotoSetSettings', 'Gallery and Collection Settings',array('ZenfolioPress','photoSetSettingsHTML'), 'ZenfolioPress');
-		add_settings_field('thumbSize', 'Default Thumbnail Size', array('ZenfolioPress','thumbSizeHTML'), 'ZenfolioPress', 'ZFP_PhotoSetSettings');
-	}
-
-	public static function zenfolioSettingsHTML() {
-		//echo '<p>Settings for accessing your Zenfolio account.</p>';
-	}
-
-	public static function userNameHTML() {
-		$options = self::getOptions();
-		echo "<input id='userName' name='ZFP_Settings[userName]' size='40' type='text' value='{$options['userName']}' />";
-	}
-
-	public static function photoSettingsHTML() {
-		//echo '<p>Default settings for photos</p>';
-	}
-
-	public static function photoSetSettingsHTML() {
-		//echo '<p>Default settings for galleries and collections</p>';
-	}
-
-	public function photoSizeHTML() {
-		$options = self::getOptions();
-		self::selectSizeHTML('photoSize', $options['photoSize']);
-	}
-
-	public function thumbSizeHTML() {
-		$options = self::getOptions();
-		self::selectSizeHTML('thumbSize', $options['thumbSize']);
-	}
-
-	public static function selectSizeHTML($name,$value) {
-		echo "<select id='$name' name='ZFP_Settings[$name]'>\n";
-		echo "<option value=\"0\"".($value == '0'?'selected':'').">Small thumbnail (up to 80 x 80)</option>\n";
-		echo "<option value=\"1\"".($value == '1'?'selected':'').">Square thumbnail (60 x 60, cropped square)</option>\n";
-		echo "<option value=\"10\"".($value == '10'?'selected':'').">Medium thumbnail (up to 120 x 120)</option>\n";
-		echo "<option value=\"11\"".($value == '11'?'selected':'').">Large thumbnail (up to 120 x 120)</option>\n";
-		echo "<option value=\"2\"".($value == '2'?'selected':'').">Small (up to 400 x 400)</option>\n";
-		echo "<option value=\"3\"".($value == '3'?'selected':'').">Medium (up to 580 x 450)</option>\n";
-		echo "<option value=\"4\"".($value == '4'?'selected':'').">Large (up to 800 x 630)</option>\n";
-		echo "<option value=\"5\"".($value == '5'?'selected':'').">X-Large (up to 1100 x 850)</option>\n";
-		echo "<option value=\"6\"".($value == '6'?'selected':'').">XX-Large (up to 1550 x 960)</option>\n";
-		echo "</select>\n";
-	}
-
-	public static function validateOptions($input) {
-		$options = self::getOptions();
-
-		/* whitelist input */
-		foreach ($input as $option=>$value) {
-			switch ($option) {
-				case 'userName':
-					$valid[$option] = trim($value);
-					break;
-				case 'photoSize':
-					if(in_array($value, array('0','1','2','3','4','5','6','10','11'))) {
-						$valid[$option] = $value;
-					}
-					break;
-				case 'thumbSize':
-					if(in_array($value, array('0','1','2','3','4','5','6','10','11'))) {
-						$valid[$option] = $value;
-					}
-					break;
-			}
-		}
-
-		/* Validate the user */
-		require_once('Zenfolio.php');
-		$zenfolio = new Zenfolio();
-		$publicProfile = $zenfolio->loadPublicProfile($valid['userName']);
-		if($publicProfile === false) {
-			//send_message('Cannot connect to Zenfolio',E_USER_ERROR);
-			$valid['userName'] = '';
-		}
-
-		self::$options = $valid;
-
-		return $valid;
-	}
-
 	public static function showPhoto($params) {
 		$options = self::getOptions();
 		$default_size = $options['photoSize'];
@@ -222,8 +127,18 @@ class ZenfolioPress {
 		require_once('Zenfolio.php');
 		$zenfolio = new Zenfolio();
 		$photo = $zenfolio->loadPhoto($id,'LEVEL2');
+		$link = $photo->PageUrl;
+		$target = $options['photoTarget'];
 		$src = 'http://'.$photo->UrlHost.'/'.$photo->UrlCore.'-'.$size.'.jpg?sn='.$photo->Sequence;
-		return "<img id=\"$id\" src=\"$src\"/>";
+		$html = '';
+		if($options['photoAction'] > 0) {
+			$html.= "<a href=\"$link\" target=\"$target\">";
+		}
+		$html.= "<img id=\"$id\" src=\"$src\"/>";
+		if($options['photoAction'] > 0) {
+			$html.= "</a>";
+		}
+		return $html;
 	}
 
 	public static function showPhotoSet($params) {
@@ -239,8 +154,9 @@ class ZenfolioPress {
 		require_once('Zenfolio.php');
 		$zenfolio = new Zenfolio();
 		$photoSet = $zenfolio->loadPhotoSet($id,'LEVEL2',true);
-
 		$photos = $photoSet->Photos;
+
+		$linkTarget = $options['thumbTarget'];
 		$tableStyle = self::getFrameTableStyle($size);
 		$cellStyle = self::getFrameCellStyle($size);
 
@@ -249,18 +165,25 @@ class ZenfolioPress {
 			$html.= "<div class=\"zfp_gallery\">\n";
 			foreach ($photos as $photo) {
 				$src = 'http://'.$photo->UrlHost.'/'.$photo->UrlCore.'-'.$size.'.jpg?sn='.$photo->Sequence;
-				//$link = $photo->PageUrl;
-				$link =  $photoSet->PageUrl.substr($photo->PageUrl,strrpos($photo->PageUrl,'/'));
-				$title = $photo->Title;
+				if($options['thumbAction'] == '2') {
+					$link =  $photoSet->PageUrl.substr($photo->PageUrl,strrpos($photo->PageUrl,'/'));
+				} else {
+					$link = $photo->PageUrl;
+				}
+				$title = $photo->Title ? $photo->Title : 'Untitled';
 				if($photo->Id == $photoSet->TitlePhoto->Id) {
 					$titleSrc = 'http://'.$photo->UrlHost.'/'.$photo->UrlCore.'-11.jpg?sn='.$photo->Sequence;
 				}
 				$html.= "<table class=\"zfpFrame\" style=\"$tableStyle\">\n";
 				$html.= "<tr class=\"zfpFrame\">\n";
 				$html.= "<td class=\"zfpFrame\" style=\"$cellStyle\">\n";
-				$html.= "<a class=\"zfpFrame\" href=\"$link\" >\n";
-				$html.= "<img src=\"$src\"/>\n";
-				$html.= "</a>\n";
+				if($options['thumbAction'] > 0) {
+					$html.= "<a class=\"zfpFrame\" href=\"$link\" target=\"$linkTarget\">\n";
+				}
+				$html.= "<img src=\"$src\"/ alt=\"$title\">\n";
+				if($options['thumbAction'] > 0) {
+					$html.= "</a>\n";
+				}
 				$html.= "</td>\n";
 				$html.= "</tr>\n";
 				/*
