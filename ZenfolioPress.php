e4df58e53d6f891063be8bb06b34bf2088ad855a
nusbaum <nusbaum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-09 01:30:27 +0000

    tagging verion 0.0.6
    git-svn-id: https://plugins.svn.wordpress.org/zenfoliopress/tags/0.0.6@435503 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ZenfolioPress.php
+++ ZenfolioPress.php
@@ -3,7 +3,7 @@
  Plugin Name: ZenfolioPress
  Plugin URI: http://www.zenfoliopress.com
  Description: Integrate Zenfolio images and galleries with Word Press.
- Version: 0.0.4
+ Version: 0.0.6
  Author: David Nusbaum
  Author URI: http://www.davidnusbaum.com
  License: GPL2
@@ -31,7 +31,8 @@ if ( is_admin() ){
 	add_action('admin_menu', array('ZenfolioPressAdmin','adminMenu'));
 	add_action('admin_init', array('ZenfolioPressAdmin','registerSettings'));
 } else {
-	add_action('wp_print_styles',array('ZenfolioPress','loadStyleSheet'));
+	add_action('wp_print_styles',array('ZenfolioPress','loadStyleSheets'));
+	add_action('wp_print_scripts',array('ZenfolioPress','loadScripts'));
 	/* add filters for photo and photoset shortcuts */
 	add_filter('widget_text', 'do_shortcode', SHORTCODE_PRIORITY,2);
 	add_shortcode('ZFP_Photo', array('ZenfolioPress','showPhoto'));
@@ -39,29 +40,11 @@ if ( is_admin() ){
 }
 
 class ZenfolioPress {
-	const STYLE_VERSION = 'v001';
+	const STYLE_VERSION = 'v002';
+	const LIGHTBOX_STYLE_VERSION = 'v2.04';
+	const LIGHTBOX_VERSION = 'v2.04';
 	private static $options = null;
 	private static $sizes = null;
-
-
-	public static function getFrameCellStyle($size) {
-		$sizes = self::getSizes();
-		$style = 'border: 0; ';
-		$style.= 'height: '.$sizes[$size][1].'px; ';
-		$style.= 'width: '.$sizes[$size][0].'px; ';
-
-		return $style;
-	}
-
-	public static function getFrameTableStyle($size) {
-		$sizes = self::getSizes();
-		$style = 'border: 0; ';
-		$style.= 'height: '.$sizes[$size][1].'px; ';
-		$style.= 'width: '.$sizes[$size][0].'px; ';
-		$style.= 'margin: 5px; ';
-
-		return $style;
-	}
 
 	public static function getOptions() {
 		if(self::$options === null) {
@@ -88,28 +71,38 @@ class ZenfolioPress {
 		return self::$options;
 	}
 
-	public static function getSizes() {
-		if(self::$sizes === null) {
-			$sizes['0'] = array(80,80);
-			$sizes['1'] = array(60,60);
-			$sizes['2'] = array(400,400);
-			$sizes['3'] = array(580,450);
-			$sizes['4'] = array(800,630);
-			$sizes['5'] = array(1100,850);
-			$sizes['6'] = array(1550,960);
-			$sizes['10'] = array(120,120);
-			$sizes['11'] = array(200,200);
-			self::$sizes = $sizes;
+	public static function loadScripts() {
+
+		$options = self::getOptions();
+		if($options['photoAction'] == '3' | $options['thumbAction'] == '3' ) {
+			wp_enqueue_script('jquery');
+			$url = plugins_url('slimbox2.js', __FILE__);
+			$fileName = WP_PLUGIN_DIR . '/zenfoliopress/slimbox2.js';
+			if ( file_exists($fileName) ) {
+				wp_register_script('zfp_slimbox2', $url,false,self::LIGHTBOX_VERSION);
+				wp_enqueue_script( 'zfp_slimbox2');
+			}
 		}
-		return self::$sizes;
 	}
 
-	public static function loadStyleSheet() {
+	public static function loadStyleSheets() {
+		/* load ZenfolioPress style sheet */
 		$url = plugins_url('style.css', __FILE__);
 		$fileName = WP_PLUGIN_DIR . '/zenfoliopress/style.css';
 		if ( file_exists($fileName) ) {
-			wp_register_style('ZFP_Style', $url,false,self::STYLE_VERSION);
-			wp_enqueue_style( 'ZFP_Style');
+			wp_register_style('zfp_style', $url,false,self::STYLE_VERSION);
+			wp_enqueue_style( 'zfp_style');
+		}
+
+		/* load lightbox style sheet if needed */
+		$options = self::getOptions();
+		if($options['photoAction'] == '3' | $options['thumbAction'] == '3' ) {
+			$url = plugins_url('slimbox2.css', __FILE__);
+			$fileName = WP_PLUGIN_DIR . '/zenfoliopress/slimbox2.css';
+			if ( file_exists($fileName) ) {
+				wp_register_style('zfp_slimbox2', $url,false,self::LIGHTBOX_STYLE_VERSION);
+				wp_enqueue_style( 'zfp_slimbox2');
+			}
 		}
 	}
 
@@ -127,15 +120,23 @@ class ZenfolioPress {
 		require_once('Zenfolio.php');
 		$zenfolio = new Zenfolio();
 		$photo = $zenfolio->loadPhoto($id,'LEVEL2');
-		$link = $photo->PageUrl;
-		$target = $options['photoTarget'];
+
+		if($options['photoAction'] == '3') {
+			$link = 'http://'.$photo->UrlHost.$photo->UrlCore.'-3.jpg?sn='.$photo->Sequence;
+		} else {
+			$link = $photo->PageUrl;
+		}
+		$lightbox = $options['photoAction'] == '3' ? 'rel="lightbox-p'.$id.'"' : '';
+		$title = $photo->Title ? 'title="'.htmlspecialchars($photo->Title).'"' : '';
+		$target = $options['photoAction'] < '3' ? 'target="'.$options['photoTarget'].'"' : '';
 		$src = 'http://'.$photo->UrlHost.'/'.$photo->UrlCore.'-'.$size.'.jpg?sn='.$photo->Sequence;
+		
 		$html = '';
-		if($options['photoAction'] > 0) {
-			$html.= "<a href=\"$link\" target=\"$target\">";
+		if($options['photoAction'] > '0') {
+			$html.= "<a href=\"$link\" $lightbox $title $target>";
 		}
 		$html.= "<img id=\"$id\" src=\"$src\"/>";
-		if($options['photoAction'] > 0) {
+		if($options['photoAction'] > '0') {
 			$html.= "</a>";
 		}
 		return $html;
@@ -156,29 +157,35 @@ class ZenfolioPress {
 		$photoSet = $zenfolio->loadPhotoSet($id,'LEVEL2',true);
 		$photos = $photoSet->Photos;
 
-		$linkTarget = $options['thumbTarget'];
-		$tableStyle = self::getFrameTableStyle($size);
-		$cellStyle = self::getFrameCellStyle($size);
+		$target = $options['thumbAction'] < '3' ? 'target="'.$options['thumbTarget'].'"' : '';
+		$lightbox = $options['thumbAction'] == '3' ? 'rel="lightbox-ps'.$id.'"' : '';
 
 		$html = '';
 		if(is_array($photos) && count($photos)) {
-			$html.= "<div class=\"zfp_gallery\">\n";
+			$html.= "<div id=\"zfp_photoset_$id\" class=\"zfp_photoset\">\n";
 			foreach ($photos as $photo) {
 				$src = 'http://'.$photo->UrlHost.'/'.$photo->UrlCore.'-'.$size.'.jpg?sn='.$photo->Sequence;
-				if($options['thumbAction'] == '2') {
-					$link =  $photoSet->PageUrl.substr($photo->PageUrl,strrpos($photo->PageUrl,'/'));
-				} else {
-					$link = $photo->PageUrl;
+				switch ($options['thumbAction']) {
+					case '2':
+						$link = $photoSet->PageUrl.substr($photo->PageUrl,strrpos($photo->PageUrl,'/'));
+						break;
+					case '3':
+						$link = 'http://'.$photo->UrlHost.$photo->UrlCore.'-3.jpg?sn='.$photo->Sequence;
+						break;
+					default:
+						$link = $photo->PageUrl;
 				}
-				$title = $photo->Title ? $photo->Title : 'Untitled';
+				$title = $photo->Title ? 'title="'.htmlspecialchars($photo->Title).'"' : '';
 				if($photo->Id == $photoSet->TitlePhoto->Id) {
-					$titleSrc = 'http://'.$photo->UrlHost.'/'.$photo->UrlCore.'-11.jpg?sn='.$photo->Sequence;
+					$titleSrc = 'http://'.$photo->UrlHost.$photo->UrlCore.'-11.jpg?sn='.$photo->Sequence;
 				}
-				$html.= "<table class=\"zfpFrame\" style=\"$tableStyle\">\n";
-				$html.= "<tr class=\"zfpFrame\">\n";
-				$html.= "<td class=\"zfpFrame\" style=\"$cellStyle\">\n";
+				//$html.= "<table class=\"zfp_frame\" style=\"$tableStyle\">\n";
+				$html.= "<table class=\"zfp_frame zfp_$size\">\n";
+				$html.= "<tr class=\"zfp_frame\">\n";
+				//$html.= "<td class=\"zfp_frame \" style=\"$cellStyle\">\n";
+				$html.= "<td class=\"zfp_frame zfp_$size\">\n";
 				if($options['thumbAction'] > 0) {
-					$html.= "<a class=\"zfpFrame\" href=\"$link\" target=\"$linkTarget\">\n";
+					$html.= "<a class=\"zfp_frame\" href=\"$link\" $lightbox $title $target>\n";
 				}
 				$html.= "<img src=\"$src\"/ alt=\"$title\">\n";
 				if($options['thumbAction'] > 0) {
@@ -186,16 +193,9 @@ class ZenfolioPress {
 				}
 				$html.= "</td>\n";
 				$html.= "</tr>\n";
-				/*
-				 if($title) {
-				 echo "<td class=\"vt_title\"><a href=\"$link\">$title</a></td>\n";
-				 } else {
-				 echo '&nbsp;';
-				 }
-				 */
 				$html.= "</table>\n";
 			}
-			$html.= "</div> <!-- /zfp_gallery -->\n";
+			$html.= "</div> <!-- /zfp_photoset -->\n";
 		}
 
 		return $html;
--- readme.txt
+++ readme.txt
@@ -4,20 +4,17 @@ Donate link: http://www.zenfoliopress.com/
 Tags: zenfolio, photo, gallery, photo gallery, photo collection, thumbnails
 Requires at least: 2.7
 Tested up to: 3.2
-Stable tag: 0.0.4
+Stable tag: 0.0.6
 
-The ZenfolioPress plugin allows users to easily incorporate their Zenfolio hosted
-photos, galleries and collections into their WordPress site.
+The ZenfolioPress plugin allows users to easily incorporate their 
+Zenfolio hosted photos, galleries and 
+collections into their WordPress site.
 
 == Description ==
 
-The ZenfolioPress plugin for WordPress allows users to easily incorporate their Zenfolio hosted
-photos, galleries and collection into their WordPress site.
-
-__Warning...__ This early release of the plugin is being posted for testing and input from potential users.
-It is quite likely that something may not work right in different hosting environment or using different 
-browsers. I will fix bug and add enhancements as quick as possible.
-
+The ZenfolioPress plugin for WordPress allows users to easily incorporate their 
+[Zenfolio](http://www.zenfolio.com) hosted photos, galleries and collection into their 
+WordPress site.
 
 == Installation ==
 
@@ -28,14 +25,14 @@ install the plugin from your WordPress dashboard -> plugins -> add new menu
 
 = Displaying Photos =
 
-Insert the code [ZFP_Photo id='nnnn'] to display a photo from your Zenfolio account. 
+Insert the code **[ZFP_Photo id='nnnn']** to display a photo from your Zenfolio account. 
 The photo id can be found at the end of the URL displayed in your web browser.
 
 > http:&#47;&#47;www.davidnusbaum.com/individuals/h6913aba#__h6913aba__
 
 = Displaying Galleries and Collections =
 
-Insert the code [ZFP_PhotoSet id='nnnn'] to display a Zenfolio gallery or collection. 
+Insert the code **[ZFP_PhotoSet id='nnnn']** to display a Zenfolio gallery or collection. 
 The id can be found at the end of the URL displayed in your web browser.
 
 > http:&#47;&#47;www.davidnusbaum.com/p**444438099**
@@ -58,6 +55,13 @@ Answer to foo bar dilemma.
 Will add soon...
 
 == Changelog ==
+= 0.0.6 =
+Integrated an optional lightbox presentation for photos, gallaries and collections. This presentation utilizes the 
+[slimbox2](http://www.digitalia.be/software/slimbox2) jquery plugin by Christophe Beyls.
+
+= 0.0.5 =
+Cleaned up the style sheet so inline styles aren't required. This should make it easier for users who want to use
+ZenfolioPress with their own styles. Continued testing with more themes to make sure there are no conflicts.
 
 = 0.0.4 =
 Added the ability to configure the link actions for photos and gallery thumbnails.
@@ -73,6 +77,15 @@ Update to fix creating settings for new installs. You'll need this to configure 
 An alpha level release published for testing and getting input from potential users.
 
 == Upgrade Notice ==
+
+= 0.0.6 =
+Integrated an optional lighbox presentation, similar to lights out, when an image is selected. Go to the options
+page and give it a try.
+
+= 0.0.5 =
+No functional changes, but removed the inline CSS so users have more flexibility when modifying the look of galleries 
+and collections.
+
 = 0.0.4 =
 This upgrade will allow you to configure the link actions for photos as well as thumbnails for galleries and collections.
 
@@ -85,4 +98,7 @@ Update to fix creating settings for new installs. You'll need this to configure 
 == To Do ==
 
 1. Add clear overlay gif to at least slow down efforts to right click and save your images.
-2. Add alternative actions for when images and thumbnails are clicked.
+1. Clicking the photo opens the “dim-the-lights” mode with the larger photo
+1. Insert a slideshow into a blog entry
+1. Strong call to action in a form of a “Buy Prints” or “Buy Products” button which will transition into the 
+buying experience.
