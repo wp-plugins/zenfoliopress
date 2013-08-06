9bfc561c3e076beb3a2d34bcefccde33b7739bd5
nusbaum <nusbaum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-06 04:04:50 +0000

    Tagging version 0.1.4
    git-svn-id: https://plugins.svn.wordpress.org/zenfoliopress/tags/0.1.4@752052 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/ZenfolioPressAdmin.php
@@ -0,0 +1,234 @@
+<?php
+class ZenfolioPressAdmin {
+
+	public static function adminMenu() {
+		add_options_page('ZenfolioPress Options', 'ZenfolioPress', 'manage_options', 'zfp_options', array('ZenfolioPressAdmin','showOptions'));
+	}
+	
+	public static function lightBoxSettingsHTML() {
+		return;
+	}
+	
+	public function lightBoxSizeHTML() {
+		$options = ZenfolioPress::getOptions();
+		self::selectSizeHTML('lightBoxSize', $options['lightBoxSize']);
+	}
+	
+	public static function lightBoxTitleHTML() {
+		$options = ZenfolioPress::getOptions();
+		$value = $options['lightBoxTitle'];
+		echo "<select id='lightBoxTitle' name='ZFP_Settings[lightBoxTitle]'>\n";
+		echo "<option value=\"None\"".($value == 'None'?'selected':'').">None</option>\n";
+		echo "<option value=\"Title\"".($value == 'Title'?'selected':'').">Photo Title</option>\n";
+		echo "<option value=\"Caption\"".($value == 'Caption'?'selected':'').">Photo Caption</option>\n";
+		echo "</select>\n";
+	}
+
+	public static function linkActionHTML($name,$value,$isPhotoSet = false) {
+		echo "<select id='$name' name='ZFP_Settings[$name]'>\n";
+		echo "<option value=\"0\"".($value == '0'?'selected':'').">None</option>\n";
+		echo "<option value=\"3\"".($value == '3'?'selected':'').">Open photo in Lightbox</option>\n";
+		if($isPhotoSet) {
+			echo "<option value=\"2\"".($value == '2'?'selected':'').">Open photo in Zenfolio collection</option>\n";
+		}
+		echo "<option value=\"1\"".($value == '1'?'selected':'').">Open photo in Zenfolio gallery</option>\n";
+		echo "</select>\n";
+	}
+
+	public static function linkTargetHTML($name,$value) {
+		echo "<select id='$name' name='ZFP_Settings[$name]'>\n";
+		echo "<option value=\"_self\"".($value == '_self'?'selected':'').">Same window</option>\n";
+		echo "<option value=\"_blank\"".($value == '_blank'?'selected':'').">New window</option>\n";
+		echo "</select>\n";
+	}
+
+	public static function photoActionHTML() {
+		$options = ZenfolioPress::getOptions();
+		self::linkActionHTML('photoAction',$options['photoAction']);
+	}
+
+	public static function photoSetSettingsHTML() {
+		echo "<p>The shortcode to display a photoset is <code>&#91;ZFP_PhotoSet id='nnnn'&#93;</code> where 'nnnn' is\n";
+		echo "the id from the end of the page URL. For example, the id for http://www.davidnusbaum.com/p<strong>646661647</strong></i>\n";
+		echo "would be 64666467. If you are using a friendly URL, you can find the id by going to the edit view.</p>\n";
+	}
+
+	public static function photoSettingsHTML() {
+		echo "<p>The shortcode to dispay a photo is <code>&#91;ZFP_Photo id='nnnn'&#93;</code> where 'nnnn' is the \n";
+		echo "id from the end of the photo page URL. For example, the id for http://www.davidnusbaum.com/portraits/h75015d5#h<strong>75015d5</strong>\n";
+		echo "would be 75015d5\n";
+	}
+	
+	public static function donateHTML() {
+		echo '<p><a href="http://zenfoliopress.com">ZenfolioPress</a> is provided by ';
+		echo '<a href="http://www.davidnusbaum.com">David Nusbaum Photography</a>. ';
+		echo 'If you like it, please consider offering a ';
+		echo '<a href="http://www.facebook.com/david.nusbaum.photography" target="_blank">like over on Facebook</a> ';
+		echo 'as an incentive for me to keep making improvements.</p>';
+	}
+
+	public function photoSizeHTML() {
+		$options = ZenfolioPress::getOptions();
+		self::selectSizeHTML('photoSize', $options['photoSize']);
+	}
+
+	public static function photoTargetHTML() {
+		$options = ZenfolioPress::getOptions();
+		self::linkTargetHTML('photoTarget',$options['photoTarget']);
+	}
+
+	public static function registerSettings() {
+		register_setting( 'ZFP_Settings', "ZFP_Settings",array('ZenfolioPressAdmin','validateOptions') );
+		add_settings_section('ZFP_ZenfolioSettings', 'Zenfolio Settings',array('ZenfolioPressAdmin','zenfolioSettingsHTML'), 'ZenfolioPress');
+		add_settings_field('userName', 'Zenfolio User Name', array('ZenfolioPressAdmin','userNameHTML'), 'ZenfolioPress', 'ZFP_ZenfolioSettings');
+
+
+		add_settings_section('ZFP_PhotoSettings', 'Default Photo Settings',array('ZenfolioPressAdmin','photoSettingsHTML'), 'ZenfolioPress');
+		add_settings_field('photoSize', 'Photo Size', array('ZenfolioPressAdmin','photoSizeHTML'), 'ZenfolioPress', 'ZFP_PhotoSettings');
+		add_settings_field('photoAction', 'Photo Link Action', array('ZenfolioPressAdmin','photoActionHTML'), 'ZenfolioPress', 'ZFP_PhotoSettings');
+		add_settings_field('photoTarget', 'Photo Link Target', array('ZenfolioPressAdmin','photoTargetHTML'), 'ZenfolioPress', 'ZFP_PhotoSettings');
+
+		add_settings_section('ZFP_PhotoSetSettings', 'Default Gallery and Collection Settings',array('ZenfolioPressAdmin','photoSetSettingsHTML'), 'ZenfolioPress');
+		add_settings_field('thumbSize', 'Thumbnail Size', array('ZenfolioPressAdmin','thumbSizeHTML'), 'ZenfolioPress', 'ZFP_PhotoSetSettings');
+		add_settings_field('thumbPadding', 'Thumbnail Padding', array('ZenfolioPressAdmin','thumbPaddingHTML'), 'ZenfolioPress', 'ZFP_PhotoSetSettings');
+		add_settings_field('thumbAction', 'Thumbnail Link Action', array('ZenfolioPressAdmin','thumbActionHTML'), 'ZenfolioPress', 'ZFP_PhotoSetSettings');
+		add_settings_field('thumbTarget', 'Thumbnail Link Target', array('ZenfolioPressAdmin','thumbTargetHTML'), 'ZenfolioPress', 'ZFP_PhotoSetSettings');
+		
+		add_settings_section('ZFP_LightBoxSettings', 'Default LightBox Settings',array('ZenfolioPressAdmin','lightBoxSettingsHTML'), 'ZenfolioPress');
+		add_settings_field('lightBoxSize', 'Photo Size', array('ZenfolioPressAdmin','lightBoxSizeHTML'), 'ZenfolioPress', 'ZFP_LightBoxSettings');
+		add_settings_field('lightBoxTitle', 'Photo Title', array('ZenfolioPressAdmin','lightBoxTitleHTML'), 'ZenfolioPress', 'ZFP_LightBoxSettings');
+		
+		add_settings_section('ZFP_Donate', 'Support ZenfolioPress',array('ZenfolioPressAdmin','donateHTML'), 'ZenfolioPress');
+	}
+
+	public static function selectSizeHTML($name,$value) {
+		echo "<select id='$name' name='ZFP_Settings[$name]'>\n";
+		echo "<option value=\"0\"".($value == '0'?'selected':'').">Small thumbnail (up to 80 x 80)</option>\n";
+		echo "<option value=\"1\"".($value == '1'?'selected':'').">Square thumbnail (60 x 60, cropped square)</option>\n";
+		echo "<option value=\"10\"".($value == '10'?'selected':'').">Medium thumbnail (up to 120 x 120)</option>\n";
+		echo "<option value=\"11\"".($value == '11'?'selected':'').">Large thumbnail (up to 200 x 200)</option>\n";
+		echo "<option value=\"2\"".($value == '2'?'selected':'').">Small (up to 400 x 400)</option>\n";
+		echo "<option value=\"3\"".($value == '3'?'selected':'').">Medium (up to 580 x 450)</option>\n";
+		echo "<option value=\"4\"".($value == '4'?'selected':'').">Large (up to 800 x 630)</option>\n";
+		echo "<option value=\"5\"".($value == '5'?'selected':'').">X-Large (up to 1100 x 850)</option>\n";
+		echo "<option value=\"6\"".($value == '6'?'selected':'').">XX-Large (up to 1550 x 960)</option>\n";
+		echo "</select>\n";
+	}
+
+	public static function showOptions() {
+		if (!current_user_can('manage_options'))  {
+			wp_die( __('You do not have sufficient permissions to access this page.') );
+		}
+		include 'options.html';
+	}
+
+	public static function thumbActionHTML() {
+		$options = ZenfolioPress::getOptions();
+		self::linkActionHTML('thumbAction',$options['thumbAction'],true);
+	}
+
+	public function thumbSizeHTML() {
+		$options = ZenfolioPress::getOptions();
+		self::selectSizeHTML('thumbSize', $options['thumbSize']);
+	}
+	
+	public function thumbPaddingHTML() {
+		$options = ZenfolioPress::getOptions();
+		$value = $options['thumbPadding'];
+		echo "<select id='thumbPadding' name='ZFP_Settings[thumbPadding]'>\n";
+		echo "<option value=\"0\"".($value == '0'?'selected':'').">0</option>\n";
+		echo "<option value=\"1\"".($value == '1'?'selected':'').">1</option>\n";
+		echo "<option value=\"2\"".($value == '2'?'selected':'').">2</option>\n";
+		echo "<option value=\"3\"".($value == '3'?'selected':'').">3</option>\n";
+		echo "<option value=\"5\"".($value == '5'?'selected':'').">5</option>\n";
+		echo "<option value=\"7\"".($value == '7'?'selected':'').">7</option>\n";
+		echo "<option value=\"10\"".($value == '10'?'selected':'').">10</option>\n";
+		echo "</select>\n";	
+	}
+
+	public static function thumbTargetHTML() {
+		$options = ZenfolioPress::getOptions();
+		self::linkTargetHTML('thumbTarget',$options['thumbTarget']);
+	}
+
+	public static function userNameHTML() {
+		$options = ZenfolioPress::getOptions();
+		echo "<input id='userName' name='ZFP_Settings[userName]' size='40' type='text' value='{$options['userName']}' />";
+	}
+
+	public static function validateOptions($input) {
+		$options = ZenfolioPress::getOptions();
+
+		/* whitelist input */
+		foreach ($input as $option=>$value) {
+			switch ($option) {
+				case 'userName':
+					$valid[$option] = trim($value);
+					break;
+				case 'photoSize':
+					if(in_array($value, array('0','1','2','3','4','5','6','10','11'))) {
+						$valid[$option] = $value;
+					}
+					break;
+				case 'photoAction':
+					if(in_array($value, array('0','1','3'))) {
+						$valid[$option] = $value;
+					}
+					break;
+				case 'photoTarget':
+					if(in_array($value, array('_self','_blank'))) {
+						$valid[$option] = $value;
+					}
+					break;
+				case 'thumbSize':
+					if(in_array($value, array('0','1','2','3','4','5','6','10','11'))) {
+						$valid[$option] = $value;
+					}
+					break;
+				case 'thumbAction':
+					if(in_array($value, array('0','1','2','3'))) {
+						$valid[$option] = $value;
+					}
+					break;
+				case 'thumbPadding':
+					if($value >= '0' && $value < '11') {
+						$valid[$option] = $value;
+					}
+					break;
+				case 'thumbTarget':
+					if(in_array($value, array('_self','_blank'))) {
+						$valid[$option] = $value;
+					}
+					break;
+				case 'lightBoxSize':
+					if(in_array($value, array('0','1','2','3','4','5','6','10','11'))) {
+						$valid[$option] = $value;
+					}
+					break;
+				case 'lightBoxTitle':
+					if(in_array($value, array('None','Title','Caption'))) {
+						$valid[$option] = $value;
+					}
+					break;
+			}
+		}
+
+		/* Validate the user */
+		require_once('Zenfolio.php');
+		$zenfolio = new Zenfolio();
+		$publicProfile = $zenfolio->loadPublicProfile($valid['userName']);
+		if($publicProfile === false) {
+			//send_message('Cannot connect to Zenfolio',E_USER_ERROR);
+			$valid['userName'] = '';
+		}
+		$valid['userName'] = $publicProfile->LoginName;
+
+		//self::$options = $valid;
+
+		return $valid;
+	}
+
+	public static function zenfolioSettingsHTML() {
+		//echo '<p>Settings for accessing your Zenfolio account.</p>';
+	}
+}
---
+++ trunk/readme.txt
@@ -0,0 +1,134 @@
+=== ZenfolioPress ===
+Contributors: nusbaum
+Donate link: http://zenfoliopress.com/
+Tags: zenfolio, photo, gallery, photo gallery, photo collection, thumbnails
+Requires at least: 2.7
+Tested up to: 3.6
+Stable tag: 0.1.4
+
+The ZenfolioPress plugin allows users to easily incorporate 
+their Zenfolio hosted photos, galleries and collections into 
+their WordPress site.
+
+== Description ==
+
+The ZenfolioPress plugin for WordPress allows users to easily incorporate their 
+[Zenfolio](http://www.zenfolio.com) hosted photos, galleries and collection into their 
+WordPress site.
+
+== Installation ==
+
+1. Extract the contents of the zip archive to the `/wp-content/plugins/` directory or 
+install the plugin from your WordPress dashboard -> plugins -> add new menu
+1. Activate the plugin through the 'Plugins menu' in WordPress
+1. Configure the plugin using the ZenfoliioPress settings page
+
+= Displaying Photos =
+
+Insert the code **[ZFP_Photo id='nnnn']** to display a photo from your Zenfolio account. 
+The photo id can be found at the end of the URL displayed in your web browser.
+
+> http:&#47;&#47;www.davidnusbaum.com/individuals/h6913aba#__h6913aba__
+
+= Displaying Galleries and Collections =
+
+Insert the code **[ZFP_PhotoSet id='nnnn']** to display a Zenfolio gallery or collection. 
+The id can be found at the end of the URL displayed in your web browser.
+
+> http:&#47;&#47;www.davidnusbaum.com/p**444438099**
+
+If you have a friendly URL for a gallery or collection you can find the id by going to the edit view.
+
+> http:&#47;&#47;www.zenfolio.com/nusbaum/p**444438099**/edit
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+Will add soon...
+
+== Changelog ==
+= 0.1.4 =
+Updated to slimbox v2.05
+
+= 0.1.3 = 
+Photo, as well as photoset, lightbox now uses the default image size. Changed the "rel" attribute so the ZenfolioPress 
+used of Slimbox does not conflict with the Slimbox plugin.
+ 
+= 0.1.2 =
+Fixed a bug that affected how titles and captions are set for photosets.
+
+= 0.1.1 =
+Quick CSS fix for themes that tie tr and td attributes to #content.
+
+= 0.1.0 =
+Fixed the space allocated for square thumbnails.
+
+= 0.0.9 =
+Added new configuration options for spacing around thumbnails and using captions rather than titles 
+for the lightbox.
+
+= 0.0.8 =
+Adjusted CSS to keep the background behind each thumbnail transparent.
+
+= 0.0.7 = 
+Minor changes to selectors in the CSS file.
+
+= 0.0.6 =
+Integrated an optional lightbox presentation for photos, gallaries and collections. This presentation utilizes the 
+[slimbox2](http://www.digitalia.be/software/slimbox2) jquery plugin by Christophe Beyls.
+
+= 0.0.5 =
+Cleaned up the style sheet so inline styles aren't required. This should make it easier for users who want to use
+ZenfolioPress with their own styles. Continued testing with more themes to make sure there are no conflicts.
+
+= 0.0.4 =
+Added the ability to configure the link actions for photos and gallery thumbnails.
+
+= 0.0.3 =
+As expected, deployment from the WordPress plugin directory had some minor impacts. Upated the path to the CSS file,
+but will look for a more dynamic way to get the path in the future.
+
+= 0.0.2 =
+Update to fix creating settings for new installs. You'll need this to configure ZenfolioPress.
+
+= 0.0.1 =
+An alpha level release published for testing and getting input from potential users.
+
+== Upgrade Notice ==
+= 0.0.8 =
+Minor CSS update to force a transparent background behind each thumbnail.
+
+= 0.0.7 = 
+Should prevent having a single column of thumbnails is certain themes.
+
+= 0.0.6 =
+Integrated an optional lighbox presentation, similar to lights out, when an image is selected. Go to the options
+page and give it a try.
+
+= 0.0.5 =
+No functional changes, but removed the inline CSS so users have more flexibility when modifying the look of galleries 
+and collections.
+
+= 0.0.4 =
+This upgrade will allow you to configure the link actions for photos as well as thumbnails for galleries and collections.
+
+= 0.0.3 =
+Required to correctly load the style sheet for the plugin.
+
+= 0.0.2 ==
+Update to fix creating settings for new installs. You'll need this to configure ZenfolioPress.
+
+== To Do ==
+
+1. Add clear overlay gif to at least slow down efforts to right click and save your images.
+1. Insert a slideshow into a blog entry
+1. Strong call to action in a form of a “Buy Prints” or “Buy Products” button which will transition into the 
+buying experience.
